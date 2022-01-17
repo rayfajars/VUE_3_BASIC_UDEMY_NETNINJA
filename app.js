f25bf9a008cebb2bdef3103e441db6f9eab41a83
Ray Fajar S <rayfajars.com>
2022-01-17 07:29:22 +0700

    first Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+  //data, funcitons,events
+  // template:'<h2>i am the template</h2>'
+  data() {
+    return {
+      title: "The Final Empira",
+      author: "Erlangga",
+      age: 45,
+    };
+  },
+});
+
+app.mount("#app");
