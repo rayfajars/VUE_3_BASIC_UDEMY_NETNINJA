63afd050f3e16e5cc266a7f3038f0dd3fb8b117a
Ray Fajar S <rayfajars.com>
2022-01-20 07:15:36 +0700

    mouse events

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ const app = Vue.createApp({
   // template:'<h2>i am the template</h2>'
   data() {
     return {
-      showBooks:true,
+      showBooks: true,
       title: "The Final Empira",
       author: "Erlangga",
       age: 45,
+      x: 0,
+      y: 0,
     };
-  },methods:{
-    toggleShowBooks(){
-      this.showBooks = !this.showBooks
-    }
-  }
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+  },
 });
 
 app.mount("#app");
