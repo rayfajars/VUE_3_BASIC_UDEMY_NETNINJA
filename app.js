b2800c8e76ffafe179d7c7f5114aea9da45ef2ae
Ray Fajar S <rayfajars.com>
2022-01-20 07:01:24 +0700

    Conditional Rendering

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const app = Vue.createApp({
   // template:'<h2>i am the template</h2>'
   data() {
     return {
+      showBooks:true,
       title: "The Final Empira",
       author: "Erlangga",
       age: 45,
     };
-  },
+  },methods:{
+    toggleShowBooks(){
+      this.showBooks = !this.showBooks
+    }
+  }
 });
 
 app.mount("#app");
