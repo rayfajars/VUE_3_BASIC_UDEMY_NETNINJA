45fe4ccc4e813a60557d7f96c741ab568a2e4a9f
Ray Fajar S <rayfajars.com>
2022-01-23 14:10:27 +0700

    v-for vue js 3

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ const app = Vue.createApp({
   // template:'<h2>i am the template</h2>'
   data() {
     return {
-      showBooks:true,
-      title: "The Final Empira",
-      author: "Erlangga",
-      age: 45,
+      showBooks: true,
+      books: [
+        {
+          title: "books abcd",
+          author: "ray",
+        },
+        {
+          title: "books abcd 2",
+          author: "ray 2",
+        },
+        {
+          title: "books abcd 3",
+          author: "ray 3",
+        },
+      ],
     };
-  },methods:{
-    toggleShowBooks(){
-      this.showBooks = !this.showBooks
-    }
-  }
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+  },
 });
 
 app.mount("#app");
