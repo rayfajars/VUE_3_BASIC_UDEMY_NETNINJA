21797ea193168f584b836f40ad7224aaee895996
Ray Fajar S <rayfajars.com>
2022-01-23 15:52:53 +0700

    filtered data by fav book with computed properties

--- app.js
+++ app.js
@@ -33,7 +33,11 @@ const app = Vue.createApp({
     },toggleFav(book){
       book.isFav = !book.isFav
     }
-  },
+  },computed:{
+    filteredBooks(){
+      return this.books.filter((book)=>book.isFav)
+    }
+  }
 });
 
 app.mount("#app");
